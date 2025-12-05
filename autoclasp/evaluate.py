73f8851f0b2aba0a495e7e85492cdc8b4a6b7419
ahmadfaiz01 <140952162+ahmadfaiz01@users.noreply.github.com>
2025-12-05 12:21:52 +0500

    Training and hyperparameter optiomization is done. Evaluation is left for the task e

---
+++ autoclasp/evaluate.py
@@ -0,0 +1,17 @@
+import time
+import numpy as np
+import pandas as pd
+
+# For evaluation metrics
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    roc_auc_score,
+    confusion_matrix,
+)
+
+# For graph plotting
+import plotly.express as px
+import plotly.graph_objs as go
